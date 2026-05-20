4d0d0720fb905664bc467aba523a0c65afce61df
jrcoak <josephrcoakley@gmail.com>
2026-05-20 18:13:23 +0000

    Set terminal scrollback to 50k lines

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ SETTINGS='{
   "workbench.startupEditor": "none",
   "workbench.sideBar.visible": false,
   "files.autoSave": "onFocusChange",
-  "editor.stickyScroll.enabled": true
+  "editor.stickyScroll.enabled": true,
+  "terminal.integrated.scrollback": 50000
 }'
 
 # Merge settings into a VS Code settings.json file
